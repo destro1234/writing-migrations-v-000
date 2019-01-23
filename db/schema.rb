a1583e4dc59596527392c759c21417c5f0ad9857
Elias Ventura <eliasventura23@gmail.com>
2019-01-23 14:31:45 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 1) do
     t.text "genre"
     t.integer "age"
     t.text "hometown"
+    t.integer "grade"
+    t.string "birthdate"
   end
 
 end
