deaed79fdbdb5b8e2ddd13aa99681967b880f634
Elias Ventura <eliasventura23@gmail.com>
2019-01-23 14:28:59 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,19 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    genre TEXT,
+    age INTEGER,
+    hometown TEXT
+  )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
